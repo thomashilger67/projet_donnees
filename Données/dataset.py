2a027b266cfac3ebad80126ccfe5ce757dbc6a2b
elenalmg <elena.loumagne@gmail.com>
2021-05-01 17:52:10 +0200

    modif elena premier mai

--- Données/dataset.py
+++ Données/dataset.py
@@ -7,7 +7,7 @@ class Dataset:
 
     
     Parameters
-    _________
+    ----------
 
     donnees_covid : Covid
         données Covid du Dataset
@@ -16,7 +16,7 @@ class Dataset:
         données Vacance du Dataset
 
     Attributs 
-    _________
+    ---------
 
     __donnees_covid : Covid
         données Covid du Dataset
@@ -25,7 +25,7 @@ class Dataset:
         données Vacance du Dataset
 
     Example 
-    _______
+    -------
 
     >>> d = Dataset(Covid('./Données/Données Covid/covid-hospit-incid-reg-2021-03-03-17h20.csv'),Vacance('./Données/vacances.json'))
     >>> print(d)
@@ -36,10 +36,42 @@ class Dataset:
 
 
     def __init__(self,donnees_covid,donnees_vacances=None):
+        '''Construit un Dataset
+
+        Parameters
+        ----------
+        donnees_covid : Covid
+            données covid du dataset
+
+        donnees_vacances : Point
+            donnees_vacances du dataset
+
+        Examples
+        --------
+        >>> d = Dataset(Covid('./Données/Données Covid/covid-hospit-incid-reg-2021-03-03-17h20.csv'),Vacance('./Données/vacances.json'))
+        '''
+
         self.__donnees_covid=donnees_covid
         self.__donnees_vacances=donnees_vacances
 
     def ajout_donnees_covid(self,new_donnee,position=-1): #la première position doit ici correspondre à 0
+        '''Ajoute une donné liée au Covid à donnees_covid
+
+        Parameters
+        ----------
+        new_donnee : list
+            nouvelle donnée à ajouter
+
+        position : int
+            position à laquelle est placée la nouvelle donnée dans la liste
+
+        Examples
+        --------
+        >>> d = Dataset(Covid('./Données/Données Covid/covid-hospit-incid-reg-2021-03-03-17h20.csv'),Vacance('./Données/vacances.json'))
+        >>> ajout_donnees_covid([2020-03-19;"Centre-Val de Loire";24;6])
+
+        '''    
+        
         if position==-1:
             self.__donnees_covid.append(new_donnee)
         else:
@@ -47,6 +79,19 @@ class Dataset:
     
     def suppr_donnees_covid(self,position=-1):
         del(self.__donnees_covid[position])
+
+    def ajout_donnees_vacances(self,cle,new_vac):
+        l=self.__donnees_vacances.dictionnaire[cle]
+        if new_vac['id']<len(l):
+            l[new_vac['id']]=new_vac
+        else :
+            l.append(new_vac)
+        d_bis={cle : l}
+        self.__donnees_vacances.update(d_bis)
+        
+    def suppr_donnees_vacances(self,cle,id):
+        del((self.__donnees_vacances.dictionnaire[cle])[id])
+
 
 
     
--- __main__.py
+++ __main__.py
@@ -1,9 +1,17 @@
 from Données.donnees_Covid import Covid
 from Données.donnees_Vacances import Vacance
+from Données.dataset import Dataset
 
 a = Covid('./Données/Données Covid/covid-hospit-incid-reg-2021-03-03-17h20.csv')
-print(a.dictionnaire)
+#print(a.dictionnaire)
 
 
-#b=Vacance('./Données/vacances.json')
+b=Vacance('./Données/vacances.json')
+
 #print(b.dictionnaire)
+
+#d={"id":0,"Description":"Vacances de Noël","DateDebut":"lundi 21 décembre 2009","DateFin":"lundi 04 janvier 2010","Zone":"Corse","annee_scolaire":"2009-2010","Debut":"2009-12-21","Fin":"2010-01-04"}
+#b.dictionnaire['Calendrier'].append(d)
+#print(b.dictionnaire['Calendrier'])
+
+
