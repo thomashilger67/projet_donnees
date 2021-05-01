91220bee8cf7e740392b69a54249b8ef48649828
Thomas Hilger <thomashilger@MacBook-Pro.local>
2021-05-01 17:52:04 +0200

    modif estimation

---
+++ Estimation/estimation_descriptive.py
@@ -0,0 +1,9 @@
+from Estimation.estimation import Estimation
+
+class EstimationDescriptive(Estimation):
+
+
+    def moyenne(self):
+        sum=0
+        for donnees in self.__donnees_Covid: 
+            sum= sum + 
--- __main__.py
+++ __main__.py
@@ -1,5 +1,5 @@
 from Données.donnees_Covid import Covid
-from Données.donnees_Vacances import Vacance
+from Données.donnees_vacances import Vacance
 
 a = Covid('./Données/Données Covid/covid-hospit-incid-reg-2021-03-03-17h20.csv')
 print(a.dictionnaire)
