7a02c4a6b249676cfcc35c05c1801e3522c30ee4
thomashilger67 <81185151+thomashilger67@users.noreply.github.com>
2021-05-01 18:43:47 +0200

    Delete Estimation directory

--- Estimation/estimation_descriptive.py
+++
@@ -1,9 +0,0 @@
-from Estimation.estimation import Estimation
-
-class EstimationDescriptive(Estimation):
-
-
-    def moyenne(self):
-        sum=0
-        for donnees in self.__donnees_Covid: 
-            sum= sum + 
