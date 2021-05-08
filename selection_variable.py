6538264162e8e2070c85dfa3fba8b4f18b2bbee4
elenalmg <elena.loumagne@gmail.com>
2021-05-08 18:43:57 +0200

    elena

--- selection_variable.py
+++ selection_variable.py
@@ -6,28 +6,27 @@ from transformation import Transformation
 
 class Selection_Var(Transformation):
 
-    def __init__(self,selection,select_fichier): #select_fichier
-        self.selection=selection
-        self.select_fichier=select_fichier
+    def __init__(self,var_selection,donnees): 
+        super().__init__(var_selection,donnees)
+      
     
     def application_Covid(self,dataset):
         new_list=[]
         covid=dataset.donnees_covid
-        indice=covid[0].index(self.selection)
+        indice=covid[0].index(self.var_selection)
         for elt in covid:
             new_list.append(elt[indice])
-
         return(new_list)
 
     def application_Vacance(self,dataset):
         vacances=dataset.donnees_vacances
         new_list=[]
-        if self.selection in vacances['Calendrier'][0]:
+        if self.var_selection in vacances['Calendrier'][0]:
             for elt in vacances['Calendrier']:
-                new_list.append(elt[self.selection])
+                new_list.append(elt[self.var_selection])
         else:
             for elt in vacances['Academie']:
-                new_list.append(elt[self.selection])
+                new_list.append(elt[self.var_selection])
         return(new_list)
                     
         
