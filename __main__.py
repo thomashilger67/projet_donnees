f6ecca6532c0665928764e300e33bf9fb669509b
Thomas Hilger <hilger.thomas67@gmail.com>
2021-05-11 19:26:38 +0200

    carte

--- __main__.py
+++ __main__.py
@@ -17,8 +17,8 @@ a=Covid('./Donnees/Donnees_Covid/donnees-hospitalieres-covid19-2021-03-03-17h03.
 b=Vacance('./Donnees/vacances.json')
 c=Covid('./Donnees/Donnees_Covid/donnees-hospitalieres-nouveaux-covid19-2021-03-03-17h03.csv')
 d=Covid('/Users/thomashilger/Desktop/projet_donnees/Donnees/Donnees_Covid/covid-hospit-incid-reg-2021-03-03-17h20.csv')
-data=Dataset(d)
-
+data=Dataset(c)
+#print(data.donnees_covid.liste[0])
 #Sauvegarder(a.liste).SauvegarderCSV('test',sep=',')
 
 
@@ -29,7 +29,7 @@ data=Dataset(d)
 
 #print(Jointure('hosp','covid',"incid_hosp",data2).application(data))
 
-print(Selection_Var('numReg','covid').application(data).donnees_covid)
+#Selection_Var('numReg','covid').application(data).donnees_covid
 #print(data)
 #Sauvegarder(Selection_Var('numReg','covid').application(data)).SauvegarderCSV('selection.csv')
 #print(Agregation_Spatiale("dc",'Covid','Occitanie').application(data))  #chiffre eh dessous des offciciels ???
@@ -42,13 +42,12 @@ print(Selection_Var('numReg','covid').application(data).donnees_covid)
 #Sauvegarder(EstimationDescriptive().moyenne(data)).SauvegarderCSV('moyenne',';')
 #print(Normalisation('numReg','covid').application(data).donnees_covid.liste)
 
-############################CARTE##########################
-cp=CartoPlot(())
+
+#############CARTE#########
+cp = CartoPlot()
+
 d = {}
-for i in range(1, 101):
-   d[str(i)] = i
-
-
-fig = cp.plot_dep_map(data=d, x_lim=(-6, 10), y_lim=(41, 52))
+d['Betagni'] = 1
+fig = cp.plot_reg_map(data=d,x_lim=(-6, 10), y_lim=(41, 52))
 fig.show()
-fig.savefig('departements.test.jpg')
+fig.savefig('regions.test.jpg')
