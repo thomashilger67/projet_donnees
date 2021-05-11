944439d862c30531b38554957dcead5b3028fd49
elenalmg <elena.loumagne@gmail.com>
2021-05-11 18:49:02 +0200

    modif

--- __main__.py
+++ __main__.py
@@ -15,11 +15,12 @@ from sauvegarder import Sauvegarder
 a=Covid('./Donnees/Donnees_Covid/donnees-hospitalieres-covid19-2021-03-03-17h03.csv')
 b=Vacance('./Donnees/vacances.json')
 c=Covid('./Donnees/Donnees_Covid/donnees-hospitalieres-nouveaux-covid19-2021-03-03-17h03.csv')
-d=Covid('/Users/thomashilger/Desktop/projet_donnees/Donnees/Donnees_Covid/covid-hospit-incid-reg-2021-03-03-17h20.csv')
-data=Dataset(d)
-print(data)
+#d=Covid('/Users/thomashilger/Desktop/projet_donnees/Donnees/Donnees_Covid/covid-hospit-incid-reg-2021-03-03-17h20.csv')
+data=Dataset(c)
+data.donnees_covid.liste[1]
+#print(data)
 #Sauvegarder(a.liste).SauvegarderCSV('test',sep=',')
-
+#Jointure('incid_hosp','vacance','Zone').application(data)
 
 #print(EstimationDescriptive().ecart_type(data))
 
@@ -28,10 +29,11 @@ print(data)
 
 #print(Jointure('hosp','covid',"incid_hosp",data2).application(data))
 
-#data=Selection_Var('numReg','covid').application(data)
-#print(data)
+data=(Selection_Var('jour','Covid').application(data))
+print(data.donnees_covid.liste[0])
+#print(data2)
 #Sauvegarder(Selection_Var('numReg','covid').application(data)).SauvegarderCSV('selection.csv')
-#print(Agregation_Spatiale("dc",'Covid','Occitanie').application(data))  #chiffre eh dessous des offciciels ???
+(Agregation_Spatiale("incid_hosp",'Covid','Occitanie').application(data))  #chiffre eh dessous des offciciels ???
 
 
 #print(EstimationMultivariee().Kmeans(data,2,2))
